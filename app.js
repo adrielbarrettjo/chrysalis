3b6f0aceb9ec4b2a36d9d1acaa30880e8cce28fb
adrielbarrettjo <galadriel@barrett-johnson.net>
2018-01-12 23:19:11 -0500

    added the first test, also travis

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// var index = require('./routes/index');
+// var users = require('./routes/users');
 
 var app = express();
+// const { PORT } = require('./bin/www');
+const { PORT } = require('./config');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +24,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+// app.use('/users', users);
+
+app.listen(process.env.PORT || 3000);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,4 +47,54 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+///////////////////////////////////////////////////////////////////////////
+
+
+// add let server, run server, and close server
+let server;
+
+function runServer(port = PORT) { //*?* why do there need to be inputs now?
+	// const port = process.env.PORT || 8080;
+	return new Promise((resolve, reject) => {
+	// 	console.log(DATABASE_URL);
+	// 	// mongoose.connect("mongodb://dbuser:dbpassword@ds245287.mlab.com:45287/blog-api-mongoose", { useMongoClient: true}, err => {
+	// 		if (err) {
+	// 			return reject(err); //*?* how does this work?
+	// 		}
+
+		server = app.listen(port, () => {
+			console.log(`Your app is listening on port ${port}`);
+			resolve (server);
+
+		})
+
+		.on('error', err => {
+			mongoose.disconnect(); //*?* why is this needed in addition to the close server?
+			// it seems like it's because the connection is within the runserver, so it needs to 
+			reject(err)
+		});
+});
+};
+
+function closeServer() {
+	// return mongoose.disconnect().then(() => { //*?* same question as above: what is the purpose of this line
+	return new Promise((resolve, reject) => {
+		console.log('closing server');
+		server.close(err => {
+			if (err) {
+				return reject(err);
+				// VS.
+				// reject(err);
+				// return;
+			}
+			resolve();
+		});
+	});
+	// });
+};
+
+if (require.main === module) {
+	runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
---
+++ config.js
@@ -0,0 +1,8 @@
+// exports.DATABASE_URL = process.env.MONGODB_URI ||
+//                        global.MONGODB_URI||
+//                       "";
+
+exports.PORT = process.env.PORT || 8080;
+
+// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+// 							"";
